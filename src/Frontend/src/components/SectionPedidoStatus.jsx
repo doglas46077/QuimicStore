21c9cd331c27863db7df3a4f7ec6bad95a2d0787
unknown <37631@fiecalunos.lan>
2026-08-31 17:21:38 -0300

    Continuação da pagina de status de pedido

--- src/Frontend/src/components/SectionPedidoStatus.jsx
+++ src/Frontend/src/components/SectionPedidoStatus.jsx
@@ -2,6 +2,9 @@ import HeaderPedido from "./Header/HeaderPedido";
 import FooterCatalogo from "./FooterCatalogo";
 import { ClipboardList } from 'reicon-react';
 import { Calendar } from 'reicon-react';
+import { AngleRight } from 'reicon-react';
+
+
 
 
 
@@ -22,60 +25,93 @@ export default function SectionPedidoStatus() {
                 </div>
 
                 <div className="flex flex-col gap-4 w-full items-center">
-                    <div className="flex bg-white h-45 w-[90%] shadow rounded-xl hover:scale-101 hover:duration-300 items-center">
+                    <div className="flex bg-white h-45 w-[90%] shadow rounded-xl hover:scale-101 hover:duration-300 items-center justify-between">
                         <div className="flex gap-8">
-                            <div className="flex ml-7 bg-emerald-100 h-25 w-25 rounded-full justify-center items-center"></div>
-                            <div className="flex flex-col border-l-2 w-70 justify-center">
+                            <div className="flex items-center h-30 w-40 border-r-2 border-gray-">
+                                <div className="flex ml-7 bg-emerald-100 h-25 w-25 rounded-full justify-center items-center"></div>
+                            </div>
+                            <div className="flex flex-col w-70 justify-center">
                                 <h1>#QS-2026-XXXX</h1>
                                 <p>Nome e Categoria</p>
-                                <div>
+                                <div className="flex gap-2">
                                     <Calendar size={24} />
                                     <p>00/00/00</p>
                                 </div>
                             </div>
                         </div>
 
-                        <div>
-                            <div></div>
-                            <div></div>
+                        <div className="flex gap-40">
+                            <div>
+                                <h1>Status</h1>
+                                <p>Total do pedido</p>
+                                <h1>R$ 00,00</h1>
+                            </div>
+                            <div className="mr-10">
+                                <div className="flex items-center justify-center h-15 w-15 bg-emerald-100 rounded-2xl">
+                                    <AngleRight size={34} />
+                                </div>
+
+                            </div>
                         </div>
                     </div>
 
-                    <div className="flex bg-white h-45 w-[90%] shadow rounded-xl hover:scale-101 hover:duration-300 items-center">
+                    <div className="flex bg-white h-45 w-[90%] shadow rounded-xl hover:scale-101 hover:duration-300 items-center justify-between">
                         <div className="flex gap-8">
-                            <div className="flex ml-7 bg-emerald-100 h-25 w-25 rounded-full justify-center items-center"></div>
-                            <div className="flex flex-col border-l-2 w-70 justify-center">
+                            <div className="flex items-center h-30 w-40 border-r-2 border-gray-">
+                                <div className="flex ml-7 bg-emerald-100 h-25 w-25 rounded-full justify-center items-center"></div>
+                            </div>
+                            <div className="flex flex-col w-70 justify-center">
                                 <h1>#QS-2026-XXXX</h1>
                                 <p>Nome e Categoria</p>
-                                <div>
+                                <div className="flex gap-2">
                                     <Calendar size={24} />
                                     <p>00/00/00</p>
                                 </div>
                             </div>
                         </div>
 
-                        <div>
-                            <div></div>
-                            <div></div>
+                        <div className="flex gap-40">
+                            <div>
+                                <h1>Status</h1>
+                                <p>Total do pedido</p>
+                                <h1>R$ 00,00</h1>
+                            </div>
+                            <div className="mr-10">
+                                <div className="flex items-center justify-center h-15 w-15 bg-emerald-100 rounded-2xl">
+                                    <AngleRight size={34} />
+                                </div>
+
+                            </div>
                         </div>
                     </div>
 
-                    <div className="flex bg-white h-45 w-[90%] shadow rounded-xl hover:scale-101 hover:duration-300 items-center">
+                    <div className="flex bg-white h-45 w-[90%] shadow rounded-xl hover:scale-101 hover:duration-300 items-center justify-between">
                         <div className="flex gap-8">
-                            <div className="flex ml-7 bg-emerald-100 h-25 w-25 rounded-full justify-center items-center"></div>
-                            <div className="flex flex-col border-l-2 w-70 justify-center">
+                            <div className="flex items-center h-30 w-40 border-r-2 border-gray-">
+                                <div className="flex ml-7 bg-emerald-100 h-25 w-25 rounded-full justify-center items-center"></div>
+                            </div>
+                            <div className="flex flex-col w-70 justify-center">
                                 <h1>#QS-2026-XXXX</h1>
                                 <p>Nome e Categoria</p>
-                                <div>
+                                <div className="flex gap-2">
                                     <Calendar size={24} />
                                     <p>00/00/00</p>
                                 </div>
                             </div>
                         </div>
 
-                        <div>
-                            <div></div>
-                            <div></div>
+                        <div className="flex gap-40">
+                            <div>
+                                <h1>Status</h1>
+                                <p>Total do pedido</p>
+                                <h1>R$ 00,00</h1>
+                            </div>
+                            <div className="mr-10">
+                                <div className="flex items-center justify-center h-15 w-15 bg-emerald-100 rounded-2xl">
+                                    <AngleRight size={34} />
+                                </div>
+
+                            </div>
                         </div>
                     </div>
 
