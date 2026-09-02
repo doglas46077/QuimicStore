b400841a13e334af1979b1c77ec06b5687dd9d93
doglasgabriell <doglasgsmacedo@gmail.com>
2026-09-02 17:08:38 -0300

    refactor: renomeando pasta BancoSQL para database

--- docs/database/quimicstoresql.sql
+++ docs/database/quimicstoresql.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `avaliacoes`
 --
 
+CREATE DATABASE IF NOT EXISTS quimicstore;
+USE quimicstore;
+
+
 DROP TABLE IF EXISTS `avaliacoes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
