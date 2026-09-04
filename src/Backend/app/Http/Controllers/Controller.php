c8fe9661a9caec4bf96398d3495bcb5e790efcd8
DOGLAS GABRIELL DOS SANTOS MACEDO <46077@fiecalunos.lan>
2026-09-04 15:24:12 -0300

    Iniciando captura das requisições do front

--- src/Backend/app/Http/Controllers/Controller.php
+++ src/Backend/app/Http/Controllers/Controller.php
@@ -4,13 +4,7 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {   
-
-    // protected => É do casa dos 3, private, public, protected. Onde a classe mãe("model") pode acessar, mas arquivos de fora não, como se fosse um intermediador
-
-    // Força o nome exato da tabela la no seeder, para o laravel fazer a "magica" de indentificar pelo plural
-    protected $table = 'logins';
-
-    // Ao sistema retornar os dados do usuario, ele retornará com a senha escondida.
+// Ao sistema retornar os dados do usuario, ele retornará com a senha escondida.
     protected $hidden = [
         'senha'
     ];
