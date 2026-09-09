617e07d3147d7eee47a819a4aa32f69b1079bd5b
doglasgabriell <doglasgsmacedo@gmail.com>
2026-09-08 23:03:27 -0300

    bd: adicionando campo de imagem em tabela produtos sql

--- docs/database/quimicstoresql.sql
+++ docs/database/quimicstoresql.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `quimicstore` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `quimicstore`;
+-- MySQL dump 10.13  Distrib 8.0.46, for Linux (x86_64)
 --
 -- Host: localhost    Database: quimicstore
 -- ------------------------------------------------------
--- Server version	8.0.45
+-- Server version	8.0.46-0ubuntu0.24.04.3
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,10 +20,6 @@
 --
 -- Table structure for table `avaliacoes`
 --
-
-CREATE DATABASE IF NOT EXISTS quimicstore;
-USE quimicstore;
-
 
 DROP TABLE IF EXISTS `avaliacoes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -190,6 +188,7 @@ CREATE TABLE `produtos` (
   `ativo` tinyint(1) NOT NULL DEFAULT '1',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `imagem` varchar(2048) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_produtos_categorias` (`categoria_id`),
   CONSTRAINT `fk_produtos_categorias` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON DELETE RESTRICT
@@ -202,7 +201,7 @@ CREATE TABLE `produtos` (
 
 LOCK TABLES `produtos` WRITE;
 /*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
-INSERT INTO `produtos` VALUES (1,1,'Béquer 250ml Vidro Borossilicato','Resistente a choques térmicos e produtos químicos.',25.50,40,1,'2026-09-02 19:22:54','2026-09-02 19:22:54'),(2,2,'Ácido Clorídrico PA 37% 1L','Grau analítico para manipulação em capela de exaustão.',89.90,12,1,'2026-09-02 19:22:54','2026-09-02 19:22:54'),(3,3,'Balança Analítica 0.0001g','Equipamento de alta precisão para calibragem e pesagem de reagentes.',1250.00,3,1,'2026-09-02 19:22:54','2026-09-02 19:22:54');
+INSERT INTO `produtos` VALUES (1,1,'Béquer 250ml Vidro Borossilicato','Resistente a choques térmicos e produtos químicos.',25.50,40,1,'2026-09-02 19:22:54','2026-09-02 19:22:54',''),(2,2,'Ácido Clorídrico PA 37% 1L','Grau analítico para manipulação em capela de exaustão.',89.90,12,1,'2026-09-02 19:22:54','2026-09-02 19:22:54',''),(3,3,'Balança Analítica 0.0001g','Equipamento de alta precisão para calibragem e pesagem de reagentes.',1250.00,3,1,'2026-09-02 19:22:54','2026-09-02 19:22:54','');
 /*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -245,4 +244,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-09-02 16:54:51
+-- Dump completed on 2026-09-08 23:02:33
