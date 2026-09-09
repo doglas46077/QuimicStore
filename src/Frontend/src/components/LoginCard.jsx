e7dfd29d0843b4f2c6599d218e1d9db6a092f3d2
VITOR HUGO LOPES DA SILVA <46100@fiecalunos.lan>
2026-09-09 16:37:32 -0300

    Finalizando Tela login

--- src/Frontend/src/components/LoginCard.jsx
+++ src/Frontend/src/components/LoginCard.jsx
@@ -3,7 +3,8 @@ import HeaderLogin from "./Header/HeaderLogin";
 function LoginCard() {
   return (
 
-    <main className="w-full h-screen flex justify-center flex-col gap-20 bg-olive-50">
+    <main className="w-full h-screen flex justify-center flex-col gap-20 bg-olive-50"
+    style={{backgroundImage: "url(assets/telaFundo.png)", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
       <HeaderLogin />
 
       <div className="flex flex-1 justify-center">
@@ -41,26 +42,32 @@ function LoginCard() {
               placeholder="Digite sua senha"
               required
               className="w-full rounded-lg border border-slate-200 px-3 py-2.5 text-sm outline-none"/>
+
+          <button className="text-sm text-emerald-700 underline  w-full text-right hover:transition hover:duration-100 hover:text-blue-800">
+            Esqueci minha senha
+          </button>
+
             <button
               type="submit"
               className="mt-6 w-full rounded-lg bg-emerald-950 py-3 text-sm font-semibold text-white hover:scale-105 hover:transition hover:duration-300">
               Entrar
             </button>
-          <button className="text-sm text-emerald-700 underline w-full text-center hover:transition hover:duration-100 hover:text-blue-800">
-            Esqueci minha senha
-          </button>
-          <button className="mt-6 w-full rounded-lg bg-emerald-950 py-3 texte-sm font-semibold text-white hover:scale-105 hover:transition hover:duration-300">
-            Cadastrar-se
-          </button>
           </form>
 
 
-          <div className="my-6 h-px w-full bg-slate-100" />
+          <div className="my-6  h-px w-full bg-slate-100" />
           <button 
             type="button" 
             className="w-full rounded-lg border border-emerald-700 py-3 text-sm font-semibold text-emerald-800 hover:scale-105 hover:transition hover:duration-300 hover:bg-emerald-800 hover:text-white">
             Voltar à loja
           </button>
+          
+          <div className="flex justify-center items-center gap-2">
+            <span>Não tem conta?</span>
+          <button className="text-sm text-emerald-700 underline  hover:transition hover:duration-100 hover:text-blue-800">
+            Registre-se aqui
+          </button>
+          </div>
 
         </div>
       </div>
