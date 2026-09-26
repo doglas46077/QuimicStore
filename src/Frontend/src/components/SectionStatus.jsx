fa4532e71cb6ce5d13d3fa4d56d5856b09de6358
gabriel37631-sys <gabriel37631@fiec.edu.br>
2026-09-26 00:41:04 -0300

    continuação da tela de status

--- src/Frontend/src/components/SectionStatus.jsx
+++ src/Frontend/src/components/SectionStatus.jsx
@@ -2,6 +2,8 @@ import HeaderLogin from "./Header/HeaderLogin";
 import FooterCatalogo from "./FooterCatalogo";
 import { ClockCircle } from 'reicon-react';
 import { CheckCircle } from 'reicon-react';
+import { ClipboardList } from 'reicon-react';
+import { Home3 } from 'reicon-react';
 
 export default function SectionStatus() {
     return (
@@ -10,8 +12,16 @@ export default function SectionStatus() {
             
             
             <div className="flex flex-col gap-3 mt-0 justify-center items-center w-full h-135 ">
-                <div className="flex justify-center w-[90%] h-[90%] bg-emerald-50 shadow rounded-2xl">
-                    <CheckCircle size={100} />
+                <div className="flex flex-col justify-center items-center w-[90%] h-[90%] bg-emerald-100 shadow rounded-2xl">
+                    <div className="flex flex-col justify-center items-center">
+                        <CheckCircle size={100} />
+                        <p className="flex justify-center text-2xl h-10 w-60">Pedido Realizado!</p>
+                        <p>Aguarde a confirmação e retire na FIEC</p>
+                    </div>
+
+                    <div className="h-[40%] w-[90%] my-5 bg-olive-50 rounded-2xl">
+
+                    </div>
                 </div>
 
                 <div className="flex justify-center items-center gap-3 w-[90%] h-10">
@@ -24,17 +34,17 @@ export default function SectionStatus() {
 
                 <div className="flex flex-col gap-5 w-[90%]">
                     
-                    <div className="flex justify-center items-center w-full h-10 bg-pink-500 rounded-lg">
-                        <div>
-
-                            <p>Não sei oq</p>
+                    <div className="flex justify-center items-center w-full h-10 text-white bg-emerald-950 rounded-lg hover:scale-101 hover:transition-transform">
+                        <div className="flex items-center gap-3">
+                            <Home3 size={30} />
+                            <p>Voltar à loja</p>
                         </div>
                     </div>
                     
-                    <div className="flex justify-center items-center w-full h-10 bg-pink-500 rounded-lg">
-                        <div>
-
-                            <p>Não sei oq</p>
+                    <div className="flex justify-center items-center w-full h-10 bg-emerald-100 rounded-lg hover:scale-101 hover:transition-transform">
+                        <div className="flex items-center gap-3">
+                            <ClipboardList size={30} />
+                            <p>Ver meus pedidos</p>
                         </div>
                     </div>
                 </div>
